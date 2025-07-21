2e4d3900b377fb953661619291659a35cdf86a9d
Nikhil Singh <nikhilpramodsingh@gmail.com>
2025-07-21 19:34:11 +0530

    update my new branch

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,14 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import JobSearchBar from "./sub-menu";
+import FeaturedJobsSection from "./featureJob";
 
 export default function Page() {
     return(
         <>
         <Navbar/>
         <JobSearchBar/>
+        <FeaturedJobsSection/>
         <Footer/>
         </>
     );
