ffe68999d34974c9b1cf00bf5599417144f25aeb
Nikhil Singh <nikhilpramodsingh@gmail.com>
2025-07-22 15:07:05 +0530

    update my new branch

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,13 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
-import JobSearchBar from "./sub-menu";
-import FeaturedJobsSection from "./featureJob";
+import CreativesSection from "../../components/Postloginpage/CreativeSection";
+import MapFilterSection from "@/components/shared/MapFilterSection";
+import FeaturedJobsSection from "@/components/shared/FeatureSection";
+import JobSearchBar from "@/components/Postloginpage/SubMenu";
+import ATSResumeSection from "@/components/Postloginpage/ATSResumeSection";
+import CompanyLogoSection from "../../components/shared/company-logo";
+import ChatShowcaseSection from "@/components/Postloginpage/ChatShowcaseSection";
+import ConnectWithRecruiterSection from "@/components/Postloginpage/ConnectWithRecruiterSection";
 
 export default function Page() {
     return(
@@ -9,6 +15,12 @@ export default function Page() {
         <Navbar/>
         <JobSearchBar/>
         <FeaturedJobsSection/>
+        <CreativesSection/>
+        <CompanyLogoSection/>
+        <MapFilterSection/>
+        <ConnectWithRecruiterSection/>
+        <ChatShowcaseSection/>
+        <ATSResumeSection/>
         <Footer/>
         </>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
-import Hero from "./Homepage/HeroSection";
-import CategoriesWithNews from "./Homepage/merge2and3";
-import ExpertsSection from "./Homepage/connection";
-import ResumeSection from "./Homepage/resume";
-import CompanyLogoSection from "./Homepage/company-logo";
-import CombinedChatJobSection from "./Homepage/liveAndchatSection";
+import Hero from "../components/Homepage/HeroSection";
+import CategoriesWithNews from "../components/Homepage/merge2and3";
+import ExpertsSection from "../components/Homepage/connection";
+import ResumeSection from "../components/Homepage/resume";
+import CompanyLogoSection from "../components/shared/company-logo";
+import CombinedChatJobSection from "../components/Homepage/liveAndchatSection";
 import Footer from "@/components/Footer";
-import PremiumSection from "./Homepage/PremiumSection";
+import PremiumSection from "../components/Homepage/PremiumSection";
 
 export default function Home() {
   return (
